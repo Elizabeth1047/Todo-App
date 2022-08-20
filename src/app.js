731b7ad38356e2006662bd89e9aa1896bf9c0e9a
Elizabeth1047 <egharevbalizzy5@gmail.com>
2022-08-20 06:53:55 -0700

    cleaning of code

--- src/app.js
+++ src/app.js
@@ -11,11 +11,19 @@ let changebg = document.getElementById("bggcolor");
 let changebgg = document.getElementById("bggwidth");
 let completed = document.getElementById("completed");
 let all = document.getElementById("all");
+let alone = document.querySelector(".alone");
 let general = document.getElementById("general");
-let delet = document.getElementById("delete");
+let nam = document.getElementById("name");
 let paragraph = document.getElementById("p");
 let count = document.getElementById("no");
+let com = document.getElementById("com");
+let alls = document.getElementById("alls");
+let actives = document.getElementById("actives");
 
+let myprompt = () => {
+  let myid = prompt("what is your name");
+  nam.innerHTML = myid + "'s";
+};
 
 toggleimage.addEventListener("click", () => {
   background.classList.toggle("bglight");
@@ -28,24 +36,27 @@ toggleimage.addEventListener("click", () => {
   changebgg.classList.toggle("bggwidth2");
   completed.classList.toggle("completed2");
   all.classList.toggle("all2");
+  alone.classList.toggle("alone2");
 });
-// creating list items
+//   // creating list items
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   let newlist = document.createElement("li");
   newlist.className = "list";
   newlist.innerHTML = `
               <div class="listchild">
-                  <div class="span"></div>
+                  <div class="span" >
+                   
+                  </div>
                   <label>${input.value}</label>
               </div>
-              <div class="img3">
-                <img src="images/icon-cross.svg" class="delete">
+              <div class="img3" id="img3">
+                <img src="images/icon-cross.svg" class="delete" id="delete">
               </div>
           </li> `;
   underlist.appendChild(newlist);
-  input.value = "";
   counts();
+  input.value = "";
 });
 
 // to remove individual list item
@@ -55,26 +66,67 @@ underlist.addEventListener("click", (e) => {
     underlist.removeChild(remove);
   }
   counts();
-// to add a checkmark to my checkbox
+  // to add a checkmark to my checkbox
   if (e.target.classList.contains("span")) {
     e.target.classList.toggle("good");
     e.target.parentElement.parentElement.classList.add("goo");
-    let imgss = document.createElement("img");
-    imgss.src = "images/icon-check.svg";
-    e.target.appendChild(imgss);
+    // let imgss = document.createElement("img");
+    // imgss.src = "images/icon-check.svg";
+    // e.target.appendChild(imgss);
   }
   // to remove list items with a checkmark
   paragraph.addEventListener("click", () => {
     for (const lis of underlist.children) {
       if (lis.classList.contains("goo")) {
+        arr = underlist.removeChild(lis);
+      }
+      counts();
+    }
+  });
+  com.addEventListener("click", () => {
+    for (const lis of underlist.children) {
+      if (!lis.classList.contains("goo")) {
         underlist.removeChild(lis);
       }
       counts();
     }
   });
 });
+
+myprompt();
+
 // to show the number of list items left after adding,deleting and removing
 let counts = () => {
   count.innerText = underlist.children.length;
 };
 
+// const loop = () => {
+//   arr.forEach((ar) => {
+//     underlist.appendChild(ar);
+//   });
+// };
+
+// underlist.addEventListener("click", (e) => {
+//   if (e.target.classList.contains("delete")) {
+//     let list = e.target.parentElement.parentElement;
+//     underlist.removeChild(list);
+//   }
+//   if (e.target.classList.contains("span")) {
+//     console.log(e.target);
+//     e.target.classList.toggle("good");
+//     e.target.parentElement.parentElement.classList.add("goo");
+//     //  underlist.firstChild.classList.toggle("spa2");
+//     loop();
+//   }
+//   counts();
+
+//   paragraph.addEventListener("click", () => {
+//     loop(){
+//     if (arr.classList.contains("goo")) {
+//       underlist.removeChild(ar);
+
+//       counts();
+//     }
+//     }
+//   });
+// });
